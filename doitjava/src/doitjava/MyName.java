018fbe3c5fad4d9ac250de8dea7066a1491ab45f
404-01 <404-01@DESKTOP-DAR98QQ>
2020-08-11 14:55:34 +0900

    자바 1일차 예제

---
+++ doitjava/src/doitjava/MyName.java
@@ -0,0 +1,31 @@
+/**
+  *(asterist)를 사용한 주석은
+  *도큐먼터리 주석으로 문서의 정보를
+  *나타낼 때 사용합니다
+  *작성자 : 금수훈
+  *작성일 : 8월 10일
+ */
+
+//한줄주석
+/*한번에 많은 주석을 넣을때*/
+/*
+  여러줄의 주석을 
+ 적으려면 이것을 
+  사용합니다
+ */
+package doitjava;
+
+public class MyName {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("제 이름은 금수훈입니다."); //화면출력코드
+		System.out.println("학원수강생입니다"); //둘째 줄의 출력코드
+	}
+    /*
+     * 첫수업시간에 작성한 코드입니다
+     * 어떤 언어를 학습할 때 최초로 화면 출력을
+     * 배우게 됩니다.
+     */
+}
+
