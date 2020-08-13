dc1e74907dee2c44f0a6f22db67b8038c08dbd8d
kumsoohun <soohunkum.dev@gmail.com>
2020-08-13 14:15:33 +0900

    chapter02 - 정수형 변수 사용법

---
+++ doitjava/src/chapter02/IntegerEx.java
@@ -0,0 +1,35 @@
+package chapter02;
+
+public class IntegerEx {
+
+	public static void main(String[] args) {
+		byte bNum = 100;
+		short sNum = 10_000;  //컴퓨터는 언더바(_)를 인식하지 않습니다.
+		short sNum2 = 10000;
+		int iNum = 1_000_000_000;
+		System.out.println(bNum);
+		System.out.println(sNum);
+		System.out.println(sNum2);
+		System.out.println(iNum);
+//		bNum =200; //byte자료형은 -128부터 127까지 사용가능합니다.
+		/*
+		 * 주석처리 단축키
+		 * 컨트롤 + 쉬프트 + C
+		 * 컨트롤 + /
+		 * 컨트롤 + 7
+		 */
+//		sNum = 40000; // -32768 ~ 32767까지 사용 가능합니다.
+
+//		iNum = 2200000000; //-2147483648 ~ 2147483647까지 사용 가능합니다.
+		System.out.println(1_000_000_000); //10억출력
+		System.out.println(3_000_000_000L); //long형 자료는 영문자 L을 붙이는데 대문자를 사용해야 합니다.
+	
+		int result;
+		result = sNum + bNum;
+		
+		long lNum = 3000000000l;
+		System.out.println(lNum);
+	
+	}
+
+}
