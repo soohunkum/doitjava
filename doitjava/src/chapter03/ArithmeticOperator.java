492e265c2014dd2e0f1f541779b4d9964a5def1b
kumsoohun <soohunkum.dev@gmail.com>
2020-08-14 16:03:51 +0900

    chapter 03 - 연산자

---
+++ doitjava/src/chapter03/ArithmeticOperator.java
@@ -0,0 +1,30 @@
+package chapter03;
+
+public class ArithmeticOperator {
+
+	public static void main(String[] args) {
+			int intResult;
+			double doubleResult;
+			
+			intResult = 3+2;
+			System.out.println(intResult);
+			intResult = 3-2;
+			System.out.println(intResult);
+			intResult = 3*2;
+			System.out.println(intResult);
+			intResult = 3/2;
+			System.out.println(intResult);
+			doubleResult = 3.0/2.0;
+			System.out.println(doubleResult);
+			intResult = 3%2;
+			System.out.println(intResult);
+	
+			doubleResult = (double)3/2;
+			System.out.println(doubleResult);
+			
+			doubleResult = 3/2;
+			System.out.println(doubleResult);
+			
+	}
+
+}
---
+++ doitjava/src/chapter03/UnaryOperator.java
@@ -0,0 +1,31 @@
+package chapter03;
+
+public class UnaryOperator {
+
+	public static void main(String[] args) {
+		int a = 10, b=10; //같은 타입은 한줄에 선언과 초기화를 할 수 있다.
+		
+		int c = 100, d = 200, e = 300;
+
+		System.out.println("a++=" + (a++) + ",++b=" + (++b));
+		System.out.println("a="+a+  ",b=" +b);
+	
+		
+		c++;  //100
+		c++;  //101
+		c++;  //102
+		System.out.println(c++);//103
+		//104
+		System.out.println(c); //104
+		
+		++c; //105
+		c++; //105
+		++c; //107
+		++c; //108
+		++c; //109
+		c++; //109
+		System.out.println(++c);//111
+	
+	}
+
+}
