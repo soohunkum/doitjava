76717c4585731aa86d33865364b6b0d2a478086a
kumsoohun <soohunkum.dev@gmail.com>
2020-08-20 15:59:30 +0900

    chapter04 - DiceEx2

--- doitjava/src/chapter04/DiceEx2.java
+++ doitjava/src/chapter04/DiceEx2.java
@@ -2,33 +2,49 @@ package chapter04;
 
 public class DiceEx2 {
 	public static void main(String[] args) {
-		int dice = (int)(Math.random() * 6) + 1;
+		int dice = (int) (Math.random() * 6) + 1;
 		System.out.println(dice);
-		
-			switch(dice) {
-			case 1:
-				System.out.println("주사위의 눈금은 1 입니다");
-				break;
-			case 2:
-				System.out.println("주사위의 눈금은 2 입니다");
-				break;
-			case 3:
-				System.out.println("주사위의 눈금은 3 입니다");
-				break;
-			case 4:
-				System.out.println("주사위의 눈금은 4 입니다");
-				break;
-			case 5:
-				System.out.println("주사위의 눈금은 5 입니다");
-				break;
-			default:
-				System.out.println("주사위의 눈금은 6 입니다");
-		
 
-				
+//			switch(dice) {
+//			case 1:
+//				System.out.println("주사위의 눈금은 1 입니다");
+//				break;
+//			case 2:
+//				System.out.println("주사위의 눈금은 2 입니다");
+//				break;
+//			case 3:
+//				System.out.println("주사위의 눈금은 3 입니다");
+//				break;
+//			case 4:
+//				System.out.println("주사위의 눈금은 4 입니다");
+//				break;
+//			case 5:
+//				System.out.println("주사위의 눈금은 5 입니다");
+//				break;
+//			default:
+//				System.out.println("주사위의 눈금은 6 입니다");
+
+		if (dice <= 3) {
+			switch (dice) {
+			case 1:
+				System.out.println("주사위의 눈금은 1입니다");
+			case 2:
+				System.out.println("주사위의 눈금은 2입니다");
+			case 3:
+				System.out.println("주사위의 눈금은 3입니다");
+			}
+		} else {
+			switch (dice) {
+			case 4:
+				System.out.println("주사위의 눈금은 4입니다");
+			case 5:
+				System.out.println("주사위의 눈금은 5입니다");
+			default:
+				System.out.println("주사위의 눈금은 6입니다");
+			}
 		}
-		
 	}
+
 }
 
 /**
