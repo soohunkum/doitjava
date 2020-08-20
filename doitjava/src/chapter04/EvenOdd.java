390fd69cfc02429e2230784714e1a6ffe548189a
kumsoohun <soohunkum.dev@gmail.com>
2020-08-20 15:44:22 +0900

    chapter04 - 짝수 홀수 판별 예제

---
+++ doitjava/src/chapter04/EvenOdd.java
@@ -0,0 +1,26 @@
+package chapter04;
+
+import java.util.Scanner;
+
+/**
+ * 숫자를 입력받아서 입력받은 숫자가 홀수인지 짝수인지 출력하세요.
+ */
+public class EvenOdd {
+	public static void main(String[] args) {
+	 
+	  	Scanner sc = new Scanner(System.in);
+	  	System.out.println("숫자를 입력해 주세요:");
+	  	System.out.print("> ");
+	  	
+	  	int Num;
+	  	Num = sc.nextInt();
+	  	
+	  	if(Num%2 == 0) {
+	  		System.out.println(Num+"은 짝수입니다");
+
+	  	}else {
+	  		System.out.println(Num+"은 홀수입니다");
+	  	}
+	  	sc.close();
+	}
+}
