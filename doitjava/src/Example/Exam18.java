3bb56e5a917e815255125c3020fb16abef412e63
kumsoohun <soohunkum.dev@gmail.com>
2020-08-27 15:49:22 +0900

    20200827

--- doitjava/src/Example/Exam18.java
+++ doitjava/src/Example/Exam18.java
@@ -2,26 +2,22 @@ package Example;
 
 import java.util.Scanner;
 
-public class Exam16 {
-
+public class Exam18 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("몇 줄을 출력할까요?:");
 		int line = sc.nextInt();
-			for (int a = 0; a < line; a++) {
-			for (int b = 0; b <= line; b++) {
-				System.out.print("");
+		for (int a = line - 1; a < 1; a--) {
+			for (int b = 0; b <= a; b++) {
+				if(a==b) {
+				System.out.print("@");
 			}else {
-			System.out.print("*");
-		}
-			
-				for(int b=0; b<a; b++) {
-				System.out.println("*");
+			System.out.print("$");
+		}  
+				
+				System.out.println();
 			}
 		sc.close();
+}
 	}
-
 }
-}
-	
-
