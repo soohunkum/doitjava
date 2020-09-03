ad00570856df3894f4d0547349612f505bfe8798
kumsoohun <soohunkum.dev@gmail.com>
2020-09-03 15:52:17 +0900

    1

--- doitjava/src/Example/Arsterisk04aa.java
+++ doitjava/src/Example/Arsterisk04aa.java
@@ -3,17 +3,31 @@ package Example;
 public class Arsterisk04aa {
 
 	public static void main(String[] args) {
-		for(int i= 0 ; i<5   ;i++) {
-			for(int j= 0 ; j<5  ; j++) {
+		for(int i= 0 ; i<5   ;i++) {     //鼻欽
+			for(int j= 0 ; j<5  ; j++) {  //謝難
 				if(4-i > j) {
-					System.out.print("");
+					System.out.print(" ");
 				}else {
 					System.out.print("*");
-			
-					
+				
 				}
 			}
-			
+			for(int j=0; j<i; j++) {  //辦難
+			System.out.print("*");
+		}
+			System.out.println();
+		}
+		for(int i=3; i>=0; i--) {//ж欽
+			for(int j=0; j<5; j++) {//謝難
+				if(4-i>j) {
+					System.out.print(" ");
+			}else {
+				System.out.print("*");
+			}
+			}
+			for(int j=0; j<i; j++) { //辦難
+				System.out.print("*");
+			}
 			System.out.println();
 		}
 	}
