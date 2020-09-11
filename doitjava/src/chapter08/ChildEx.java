f31e0990faaca4bb5a3218d22956cff72b927288
soohunkum <soohunkum.dev@gmail.com>
2020-09-11 15:44:12 +0900

    33

---
+++ doitjava/src/chapter08/ChildEx.java
@@ -0,0 +1,17 @@
+package chapter08;
+
+public class ChildEx {
+
+	public static void main(String[] args) {
+	
+			Child child = new Child();
+			
+			Parent parent = child;
+			parent.method1();
+			parent.method2();
+			//parent.method3()호출 불가능
+		}
+
+	}
+
+
---
+++ doitjava/src/chapter08/SupersonicAirplaneEx.java
@@ -0,0 +1,17 @@
+package chapter08;
+
+public class SupersonicAirplaneEx {
+
+	public static void main(String[] args) {
+		SupersonicAirplane sa = new SupersonicAirplane();
+		sa.takeoff();
+		sa.fly();
+		sa.flyMode = SupersonicAirplane.SUPERSONIC;
+		sa.fly();
+		sa.flyMode = SupersonicAirplane.NORMAL;
+		sa.fly();
+		sa.land();
+
+	}
+
+}
