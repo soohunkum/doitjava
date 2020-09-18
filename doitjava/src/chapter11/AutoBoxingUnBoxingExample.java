a38ffef19fc720e2de85f5945fa5fc3e7d5f6c6d
soohunkum <soohunkum.dev@gmail.com>
2020-09-18 13:26:26 +0900

    ss

---
+++ doitjava/src/chapter11/AutoBoxingUnBoxingExample.java
@@ -0,0 +1,20 @@
+package chapter11;
+
+public class AutoBoxingUnBoxingExample {
+
+	public static void main(String[] args) {
+		//자동 Boxing
+		Integer obj = 100;
+		System.out.println("value:" + obj.intValue());
+		
+		//대입시 자동 UnBoxing
+		int value = obj;
+		System.out.println("value:" + value);
+		
+		//연산시 자동 UnBoxing
+		int result = obj + 100;
+		System.out.println("result:" + result);
+
+	}
+
+}
