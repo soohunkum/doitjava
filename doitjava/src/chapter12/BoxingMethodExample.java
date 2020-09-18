d5e9f65fa4d0d011d4bebb69160818db6c08931f
soohunkum <soohunkum.dev@gmail.com>
2020-09-18 16:03:44 +0900

    33

--- doitjava/src/chapter12/BoxingMethodExample.java
+++ doitjava/src/chapter12/BoxingMethodExample.java
@@ -5,9 +5,11 @@ public class BoxingMethodExample {
 	public static void main(String[] args) {
 			Box<Integer> box1 = Util.<Integer>boxing(100);
 			int intValue = box1.get();
+			System.out.println(intValue);
 			
 			Box<String> box2 = Util.boxing("ȫ�浿");
 			String strValue = box2.get();
+			System.out.println(strValue);
 
 	}
 
