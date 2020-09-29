bb32e8f57ab7106eeb3284d4f8068f7f75797d55
soohunkum <soohunkum.dev@gmail.com>
2020-09-29 14:39:34 +0900

    33

--- doitjava/src/chapter13/stream/Student.java
+++ doitjava/src/chapter13/stream/Student.java
@@ -1,13 +1,25 @@
 package chapter13.stream;
 
-public class Student {
-		private String name;
-		private int score;
-		
-		public Student (String name, int score) {
-			this.name = name;
-			this.score = score;
-		}
-		public String getName() {return name;}
-		public int getScore() {return score;}
+public class Student implements Comparable<Student> {
+	private String name;
+	private int score;
+
+	public Student(String name, int score) {
+		this.name = name;
+		this.score = score;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public int getScore() {
+		return score;
+	}
+
+	@Override // Comparable 인터페이스의 추상 메소드
+	public int compareTo(Student o) {
+		return Integer.compare(this.score, o.score);
+	}
+
 }
